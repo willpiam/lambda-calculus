aa6df62cffbaaec0fac591ade62d447f438f6948
William <willdoyle422@gmail.com>
2023-12-20 14:55:04 -0500

    more lambda calculus

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const M = f => f(f)                         // Mockingbird  (self-application)
 
 console.assert(M(I) === I, 'The Mocking bird of the Idiot is the Idiot') 
 
+const K = a => b => a                      // Kestrel (const)
+
+console.assert(K(I)(M) === I, 'The Kestrel of the Idiot and the Mockingbird is the Idiot')
+
+const KI = K(I)                             // Kite (or λab.b)
+
+console.assert(KI(K)(M) === M, 'The Kite of the Mockingbird is the Mockingbird')
+
+const C = f => a => b => f(b)(a)           // Cardinal (flip)
+
+console.assert(C(K)(I)(M) === M, 'The Cardinal of the Kestrel, the Idiot and the Mockingbird is the Mockingbird')
+
 const ONE = f => a => f(a)
 
 const SUCC = n => f => a => f(n(f)(a))
